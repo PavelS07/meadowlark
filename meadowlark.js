7eb95da88348df3f57ac12194b91a6facbaba85a
Павел Соболев <pavelsobolev@imac-op-03.local>
2021-03-11 18:56:13 +0300

    Add partials - weather.handlebars

--- meadowlark.js
+++ meadowlark.js
@@ -5,8 +5,8 @@ let app = express();
 // установка механизма представления handlebars
 let handlebars = require('express-handlebars').create(
     { defaultlayout:'main',
-            helpers:{
-            section:function(name, options){
+             helpers:{
+               section:function(name, options){
                 if(!this._sections){this._sections = {}};
                 this._sections[name] = options.fn(this);
                 return null;
@@ -15,6 +15,7 @@ let handlebars = require('express-handlebars').create(
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+// порт для прослушки
 app.set('port', process.env.PORT || 3000);
 
 // определяем папку со статическими данными
@@ -24,6 +25,13 @@ app.use(express.static(__dirname + '/public'))
 // установка тестов
 app.use(function(req, res, next){
     res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
+
+// промежуточное ПО для примера работы частичных шаблонов
+app.use(function(req, res, next){
+    if(!res.locals.partials) res.locals.partials = {};
+    res.locals.partials.weatherContext = getWeatherData();
     next();
 });
 
@@ -64,7 +72,7 @@ app.use(function(req, res){
 });
 
 // пользовательская страница 500
-app.use(function(req, res){
+app.use(function(err, req, res){
     console.error(err.stack);
     res.status(500);
     res.render('500');
@@ -74,3 +82,32 @@ app.use(function(req, res){
 app.listen(app.get('port'), function(){
     console.log('Express запущен\nНажмите Ctrl+C для завершения.');
 });
+
+// функция для работы примера с частичными шаблонами
+function getWeatherData(){
+    return {
+        locations: [
+            {
+                name: 'Портленд',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Сплошная облачность ',
+                temp: '54.1 F (12.3 C)',
+            },
+            {
+                name: 'Бенд',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Малооблачно',
+                temp: '55.0 F (12.8 C)',
+            },
+            {
+                name: 'Манзанита',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Небольшой дождь',
+                temp: '55.0 F (12.8 C)',
+            },
+        ],
+    }
+}
